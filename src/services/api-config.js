d9065b5afeefa0d822c3a2646bd4720e30d46733
Andy Bayer <39823924+arbayer4@users.noreply.github.com>
2022-05-11 16:49:01 -0400

    cors anywhere

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseURL = process.env.NODE_ENV === "production" ? "https://git.heroku.com/wdr-bloomington.git" : "http://localhost:3000";
+const baseURL = process.env.NODE_ENV === "production" ? "ttps://cors-anywhere.herokuapp.com/https://git.heroku.com/wdr-bloomington.git" : "http://localhost:3000";
 
 const API = axios.create({baseURL: baseURL,});
 
